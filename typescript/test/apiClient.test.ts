5b1c3541c3dcc6480b42444fcaec0fe50f4bb2a5
ccHarvestasya <harvestasya84@gmail.com>
2025-11-24 10:47:44 +0900

    インポートパスを修正して、srcディレクトリからAccountRoutesApiを取得するように変更

--- typescript/test/apiClient.test.ts
+++ typescript/test/apiClient.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { AccountRoutesApi, Configuration } from "../dist";
+import { AccountRoutesApi, Configuration } from "../src";
 
 describe("Generated API Client", () => {
   const client = new AccountRoutesApi(
