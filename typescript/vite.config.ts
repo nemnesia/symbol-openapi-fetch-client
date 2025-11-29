82fa47ecf135b46c5b8ee8db0a8ab182da208691
ccHarvestasya <harvestasya84@gmail.com>
2025-11-29 10:30:49 +0900

    依存パッケージとビルドの更新

--- typescript/vite.config.ts
+++ typescript/vite.config.ts
@@ -1,34 +1,21 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'NemSymbolWebSocketMonitor',
-      formats: ['es', 'cjs', 'umd'],
-      fileName: (format) => `nem-symbol-websocket-monitor.${format}.js`,
-    },
-    outDir: 'dist/umd',
-    rollupOptions: {
-      // 外部依存は必要に応じて調整
-      external: ['@stomp/stompjs', 'isomorphic-ws', 'ws'],
-      output: {
-        globals: {
-          '@stomp/stompjs': 'StompJs',
-          'isomorphic-ws': 'WebSocket',
-          ws: 'Ws',
-        },
+export default defineConfig(({ mode }) => {
+  const isMinify = mode === 'production';
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'SymbolOpenApiFetchClient',
+        formats: ['es', 'cjs', 'umd'] as const,
+        fileName: (format) => `symbol-openapi-fetch-client.${format}${isMinify ? '.min' : ''}.js`,
       },
+      outDir: 'dist/browser',
+      target: 'es2018',
+      emptyOutDir: false,
+      sourcemap: true,
+      minify: isMinify,
     },
-    target: 'es2018',
-    emptyOutDir: false,
-  },
-  plugins: [
-    dts({
-      tsconfigPath: './tsconfig.umd.json',
-      outDir: 'dist/umd/types',
-    }),
-  ],
+  };
 });
